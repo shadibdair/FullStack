5469abb1a19f8d46fdda0a341ca648003f2fa8f2
shadibdair <43513994+shadibdair@users.noreply.github.com>
2019-03-31 00:41:54 +0300

    Add files via upload

---
+++ 08_Angular/Day 02 - 26.12.2018/01_add new component/proj/src/app/animals/animals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animals',
+  templateUrl: './animals.component.html',
+  styleUrls: []
+})
+export class AnimalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
